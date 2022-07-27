24e0e7a47f5412ad79cffc134f5970c2ab74d816
hanzalah99 <hanzalahqayyum.ee@gmail.com>
2022-07-28 02:24:49 +0500

    Update main.c

--- main/main.c
+++ main/main.c
@@ -52,9 +52,9 @@ void app_main(void)
     // calculating the angles of quaterinons from the euler angles
     array = quaternions(roll_r, pitch_r, yaw_r);
 
-    for (int i = 0; i < 4;i++)
-    {
-        printf("q[%d] : %f \n",i, array[i]);
-    }
+        for (int i = 0; i < 4;i++)
+        {
+            printf("q[%d] : %f \n",i, array[i]);
+        }
     }
     }
